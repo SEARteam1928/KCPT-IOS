7d720011f3d0a97c5d315a3d1eb5c69d33096b8f
Студент <student@iMac-312-5-2019.local>
2019-12-12 18:04:28 +0500

    firebasewithoutcocoanotwork

--- KCPT-IOS/ViewController.swift
+++ KCPT-IOS/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
+    var select = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func testButton(_ sender: Any) {
+        if(select == 0){
+            select = 1
+        testLabel.text = "Иван Павлович, дайте пароль от sudo"
+        }
+        if(select == 1){
+            select = 0
+            testLabel.text = "Label"
+        }
+    }
+    
 }
 
